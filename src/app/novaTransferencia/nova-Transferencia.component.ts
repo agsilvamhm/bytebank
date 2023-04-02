a124286f1d572dadf28983a5b695c5b8e25dec37
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2023-04-02 08:36:39 -0300

    Registrando a entrada no app.module

---
+++ src/app/novaTransferencia/nova-Transferencia.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-nova-Transferencia',
+  templateUrl: './nova-Transferencia.component.html',
+  styleUrls: ['./nova-Transferencia.component.scss']
+})
+
+export class NovaTransferenciaComponent {
+
+}
